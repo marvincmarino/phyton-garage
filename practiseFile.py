f28eb5ce3802064fb4c4754bf66d87be2c3c146b
Marvin Marino <marvin.marino.mailbox@gmail.com>
2015-12-23 17:18:25 +0100

    reverser working locally

--- practiseFile.py
+++ practiseFile.py
@@ -3,17 +3,16 @@
 # reverse string:
 # given a input string the code should invert it
 
-def reverser(text):
-    inverted_text = []
+def reverse(text):
+    inverted_text = ""
     text_len = len(text)
-    for i in range(text_len, 0):
-        for j in range(1, text_len):
-            inverted_text.append(text[j])
-            i -= 1
-            j += 1
+    for i in range(0, text_len):
+        inverted_text = inverted_text + text[text_len-i-1] # looping backward
+        print "iteration: %i, letter: %s " % (i,inverted_text)
+        i += 1
     print(inverted_text)
 
 
 # user input
 text = raw_input("Insert a text you want to revert:")
-reverser(text)
+reverse(text)
