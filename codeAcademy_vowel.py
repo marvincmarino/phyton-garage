e24b63b565f4bff6d7fa5fea3b918f7feadd03e3
Marvin Marino <marvin.marino.mailbox@gmail.com>
2015-12-23 20:08:52 +0100

    final version including working code for the site

--- codeAcademy_vowel.py
+++ codeAcademy_vowel.py
@@ -10,7 +10,17 @@ def anti_vowel(text):
             continue
         final_string.append(original_string[i])
         i += 1
-    print "".join(final_string)
+    print "".join(final_string) # creates the string by joining the list elements
 
 user_input = str(raw_input("Insert string you wish to remove the vowels: "))
 anti_vowel(user_input)
+
+# Below a working version to post on the site:
+
+def anti_vowel(text):
+    vowel = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    res=[]
+    for i in text:
+        if i not in vowel:
+            res.append(i)
+    return ''.join(res)
